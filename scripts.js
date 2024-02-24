77afecb88364acde8854c3a1332b4d81ee608211
Alex <schwimmeralexander@gmail.com>
2024-02-23 20:21:18 -0700

    Include a function to play a game and a function to receive user input
    The function to play a game evaluates user input against the computer's
    random selection
    The function to receive user input validates the string user enters in
    prompt
    THEN calls function to generate random computer input
    THEN calls function with user input and computer input as params to
        determine a winner

--- scripts.js
+++ scripts.js
@@ -10,3 +10,44 @@ function getComputerChoice() {
     }
     return choice;
 }
+
+function playGame(playerSelection, computerSelection) {    
+    if (playerSelection === "rock") {
+        if(computerSelection === "paper") {
+            console.log(`You picked ${playerSelection}. Computer picked ${computerSelection}. You lose.`)
+        }
+        else if (computerSelection === "scissors") {
+            console.log(`You picked ${playerSelection}. Computer picked ${computerSelection}. You win.`)
+        } else {
+            console.log(`You picked ${playerSelection}. Computer picked ${computerSelection}. Tie.`)
+        }
+    }
+    if (playerSelection === "paper") {
+        if(computerSelection === "scissors") {
+            console.log(`You picked ${playerSelection}. Computer picked ${computerSelection}. You lose.`)
+        }
+        else if (computerSelection === "rock") {
+            console.log(`You picked ${playerSelection}. Computer picked ${computerSelection}. You win.`)
+        } else {
+            console.log(`You picked ${playerSelection}. Computer picked ${computerSelection}. Tie.`)
+        }
+    }
+    if (playerSelection === "scissors") {
+        if(computerSelection === "rock") {
+            console.log(`You picked ${playerSelection}. Computer picked ${computerSelection}. You lose.`)
+        }
+        else if (computerSelection === "paper") {
+            console.log(`You picked ${playerSelection}. Computer picked ${computerSelection}. You win.`)
+        } else {
+            console.log(`You picked ${playerSelection}. Computer picked ${computerSelection}. Tie.`)
+        }
+    }
+}
+function playerInput() {
+    let playerChoice = undefined;
+    while(playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
+        playerChoice = prompt("Enter 'rock' 'paper' or 'scissors' to play a game").toLowerCase();
+    }
+    const computerChoice = getComputerChoice();
+    playGame(playerChoice, computerChoice);
+}
